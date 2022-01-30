6c2487199add70c2fc18383ff7ec57eea23bc54a
parayaticblue <paraya@ticblue.com>
2022-01-30 03:40:21 -0300

    Lazy básico Listo

--- src/01-Lazyload/router/routes.ts
+++ src/01-Lazyload/router/routes.ts
@@ -1,31 +1,44 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
+import { lazy, LazyExoticComponent } from 'react';
+// import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
+
+// Generamos un nuevo tipo personalizado para que el tipo Component pueda recibir un Compoenente Lazy o un compoenente JSX.Element
+type JSXComponent = () => JSX.Element;
 
 // Definimos tipos de datos para las rutas
 interface Route {
   to: string;
   path: string;
-  Component: () => JSX.Element;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
   name: string;
 }
 
+// Implementación de LazyLoad - debemos exportar los componentes por defecto
 
+const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */'../pages/LazyPage1'));
+const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */'../pages/LazyPage2'));
+const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */'../pages/LazyPage3'));
+
+// webpackChunkName : nos permite cambiar los nombres a los archivos chunks
+
+
+// Definimos las rutas
 export const routes: Route[] = [
   {
     to: '/lazy1',
     path: 'lazy1',
-    Component: LazyPage1,
+    Component: Lazy1,
     name: 'Lazy-1'
   },
   {
     to: '/lazy2',
     path: 'lazy2',
-    Component: LazyPage2,
+    Component: Lazy2,
     name: 'Lazy-2'
   },
   {
     to: '/lazy3',
     path: 'lazy3',
-    Component: LazyPage3,
+    Component: Lazy3,
     name: 'Lazy-3'
   },
 ]
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,5 @@
 
+import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import { routes } from '../01-Lazyload/router/routes';
@@ -8,44 +9,47 @@ import logo from '../logo.svg';
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="react-logo"></img>
-          <ul>
+    <Suspense fallback={<span>Loading...</span>}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="react-logo"></img>
+            <ul>
+              {
+                routes.map(({ to, name }) => (
+                  <li key={to}>
+                    <NavLink
+                      to={to}
+                      className={({ isActive }) => isActive ? 'nav-active' : ''}
+                    >
+                      {name}
+                    </NavLink>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
+          <Routes>
             {
-              routes.map(({ to, name }) => (
-                <li key={to}>
-                  <NavLink
-                    to={to}
-                    className={({ isActive }) => isActive ? 'nav-active' : ''}
-                  >
-                    {name}
-                  </NavLink>
-                </li>
+              routes.map(({ to, path, Component }) => (
+                <Route
+                  key={to}
+                  path={path}
+                  element={<Component />}
+                />
               ))
             }
-          </ul>
-        </nav>
-        <Routes>
-          {
-            routes.map(({ to, path, Component }) => (
-              <Route
-                key={to}
-                path={path}
-                element={<Component />}
-              />
-            ))
-          }
 
-          <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
-
-        </Routes>
-      </div>
-    </BrowserRouter>
+            <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
 // Routes cambio por Switch
 // NavLink cambio por Link --> className={({ isActive }) => isActive ? 'nav-active' : ''} end
 // <Routes> : cabio en la forma de renderizar los componentes
+
+// <Suspense> : cambio por Suspense este sirve para que cuando cargue un componente haz lo siguiente 
