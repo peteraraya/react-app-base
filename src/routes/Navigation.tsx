4bd61ac870e898683fe0926dbaed278aa5697616
parayaticblue <paraya@ticblue.com>
2022-01-30 03:12:41 -0300

    se añade refactorización de rutas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-Lazyload/pages';
+import { routes } from '../01-Lazyload/router/routes';
 
 
 import logo from '../logo.svg';
@@ -13,22 +13,33 @@ export const Navigation = () => {
         <nav>
           <img src={logo} alt="react-logo"></img>
           <ul>
-            <li>
-              <NavLink to="/lazy1" className={ ( {isActive} ) => isActive ? 'nav-active' : '' } >Lazy1</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2" className={ ( {isActive} ) => isActive ? 'nav-active' : '' }>Lazy2</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3" className={ ( {isActive} ) => isActive ? 'nav-active' : '' }>Lazy3</NavLink>
-            </li>
+            {
+              routes.map(({ to, name }) => (
+                <li key={to}>
+                  <NavLink
+                    to={to}
+                    className={({ isActive }) => isActive ? 'nav-active' : ''}
+                  >
+                    {name}
+                  </NavLink>
+                </li>
+              ))
+            }
           </ul>
         </nav>
         <Routes>
-          <Route path="lazy1" element={<LazyPage1 />} />
-          <Route path="lazy2" element={<LazyPage2 />} />
-          <Route path="lazy3" element={<LazyPage3 />} />
-          <Route path="/*" element={<Navigate to="/lazy1" replace /> } />
+          {
+            routes.map(({ to, path, Component }) => (
+              <Route
+                key={to}
+                path={path}
+                element={<Component />}
+              />
+            ))
+          }
+
+          <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+
         </Routes>
       </div>
     </BrowserRouter>
